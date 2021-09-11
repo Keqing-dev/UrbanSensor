ff71775b66a75e33308dfcd805225a5ec6af32f9
mguti <m.gutierrez.tolorza@gmail.com>
2021-09-10 21:18:03 -0300

    + Se crea UploadsController para manejar visualización de archivos + Se agrega JsonSerializer con converter de url en avatar y file + Se agrega rutas uploads a WebSecurityConfig

---
+++ src/main/java/dev/keqing/urbansensor/controller/UploadsController.java
@@ -0,0 +1,54 @@
+package dev.keqing.urbansensor.controller;
+
+import dev.keqing.urbansensor.exception.CustomException;
+import dev.keqing.urbansensor.service.FileStorageService;
+import dev.keqing.urbansensor.utils.FileType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("uploads")
+public class UploadsController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @GetMapping("/{type}/{filename}")
+    ResponseEntity<Resource> loadFile(@PathVariable String filename, @PathVariable String type, HttpServletRequest request) throws CustomException {
+        Resource resource;
+
+        switch (type) {
+            case "avatar":
+                resource = fileStorageService.loadFileAsResource(filename, FileType.AVATAR);
+                break;
+            case "file":
+                resource = fileStorageService.loadFileAsResource(filename, FileType.FILE);
+                break;
+            default:
+                throw new CustomException(HttpStatus.NOT_FOUND);
+        }
+
+        String contentType;
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, "No se pudo determinar el tipo de archivo");
+        }
+
+        if(contentType == null)
+            contentType = "application/octet-stream";
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+    }
+}
---
+++ src/main/java/dev/keqing/urbansensor/utils/FileURLConverter.java
@@ -0,0 +1,18 @@
+package dev.keqing.urbansensor.utils;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import dev.keqing.urbansensor.config.GeneralConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileURLConverter extends StdConverter<String, String> {
+
+    @Autowired
+    private GeneralConfig generalConfig;
+
+    @Override
+    public String convert(String value) {
+        return generalConfig.getDomainName() + "uploads/file/" + value;
+    }
+}
